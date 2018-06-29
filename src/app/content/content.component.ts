f9f76c36ed0e5300f825edee282f066103281220
Fabián <fabian@kibostudios.com>
2018-06-29 19:24:04 +0200

    Test Ebury - Añadiendo validaciones y gestión de imágenes

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, MaxLengthValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-content',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+    public emailForm: FormGroup;
+    public images: Array<any>;
+    private EMAILS_REGEX = /^(\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]{2,4}\s*?,?\s*?)+$/;
+    private MAX_SUBJECT = 100;
 
-  ngOnInit() {
-  }
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit() {
+        this.createForm();
+        this.images = [];
+    }
+
+    private createForm() {
+        this.emailForm = this.fb.group({
+            to: ['', [Validators.required, Validators.pattern(this.EMAILS_REGEX)]],
+            cc: ['', Validators.pattern(this.EMAILS_REGEX)],
+            bcc: ['', Validators.pattern(this.EMAILS_REGEX)],
+            subject: ['', [Validators.required, Validators.maxLength(this.MAX_SUBJECT)]],
+            message: ['', Validators.required]
+        });
+    }
+
+    public uploadFile(event) {
+        if (event.target.files && event.target.files[0]) {
+            const reader = new FileReader();
+            reader.readAsDataURL(event.target.files[0]);
+            reader.onload = (e) => {
+                this.images.push(e.target.result);
+            };
+        }
+    }
 
 }
