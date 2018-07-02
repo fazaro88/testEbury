3dca49124c8400121e76707897e3c6c06662affe
fazaro88 <38842092+fazaro88@users.noreply.github.com>
2018-07-02 12:31:14 +0200

    Fix error de no subida
    Añadido correción de la no subida del último commit

---
+++ content.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss']
+})
+export class ContentComponent implements OnInit {
+
+    public emailForm: FormGroup;
+    public images: Array<any>;
+    public showEmail: boolean;
+    private EMAILS_REGEX = /^(\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]{2,4}\s*?,?\s*?)+$/;
+    private MAX_SUBJECT = 100;
+
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit() {
+        this.createForm();
+        _.set(this, 'showEmail', false);
+        _.set(this, 'images', []);
+    }
+
+    private createForm() {
+        this.emailForm = this.fb.group({
+            to: ['', [Validators.required, Validators.pattern(this.EMAILS_REGEX)]],
+            cc: ['', Validators.pattern(this.EMAILS_REGEX)],
+            bcc: ['', Validators.pattern(this.EMAILS_REGEX)],
+            subject: ['', [Validators.required, Validators.maxLength(this.MAX_SUBJECT)]],
+            message: ['', Validators.required]
+        });
+    }
+
+    public uploadFile(event) {
+        _.set(this, 'images', []);
+        if (!_.isEmpty(event.target.files)) {
+            _.forEach(event.target.files, (file: any) => {
+                const reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = (e) => {
+                    this.images.push(e.target.result);
+                };
+            });
+        }
+    }
+
+    public removeFile(pos: number) {
+        this.images.splice(pos, 1);
+    }
+
+    public sendEmail() {
+        _.set(this, 'showEmail', true);
+    }
+}
